03a8c2aa19896d00f0da5505e95836c823d37e26
Wilmer Barrios <wilmerbt@yahoo.com>
2021-04-02 14:20:26 -0600

    `EssentialCommentsFeedTests` target added

---
+++ EssentialCommentsFeed/EssentialCommentsFeedTests/EssentialCommentsFeedTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialCommentsFeedTests.swift
+//  EssentialCommentsFeedTests
+//
+//  Created by Wilmer Barrios on 02/04/21.
+//
+
+import XCTest
+
+class EssentialCommentsFeedTests: XCTestCase {
+
+    func test_init() {
+//        XCTFail()
+    }
+}
