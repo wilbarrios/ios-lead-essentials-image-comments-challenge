08337a725bb24e79bb1b0e13a1c8bbea252a0a60
Wilmer Barrios <wilmerbt@yahoo.com>
2021-04-04 18:04:14 -0500

    `EssentialCommentsFeediOS` target with tests added

---
+++ EssentialCommentsFeed/EssentialCommentsFeediOS/EssentialCommentsFeediOS.h
@@ -0,0 +1,18 @@
+//
+//  EssentialCommentsFeediOS.h
+//  EssentialCommentsFeediOS
+//
+//  Created by Wilmer Barrios on 04/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialCommentsFeediOS.
+FOUNDATION_EXPORT double EssentialCommentsFeediOSVersionNumber;
+
+//! Project version string for EssentialCommentsFeediOS.
+FOUNDATION_EXPORT const unsigned char EssentialCommentsFeediOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialCommentsFeediOS/PublicHeader.h>
+
+
