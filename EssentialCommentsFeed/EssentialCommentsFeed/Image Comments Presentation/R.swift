98b72e72847ff2b59f08b167257bfa37f0270398
Wilmer Barrios <wilmerbt@yahoo.com>
2021-04-03 22:07:22 -0600

    `IImageCommentsFeedViewModel` title is localized

--- EssentialCommentsFeed/EssentialCommentsFeed/Image Comments Presentation/R.swift
+++ EssentialCommentsFeed/EssentialCommentsFeed/Image Comments Presentation/R.swift
@@ -5,3 +5,21 @@
 //  Created by Wilmer Barrios on 03/04/21.
 //
 
+import Foundation
+
+internal class R {
+    private static let table = "ImageComments"
+    
+    struct ImmageCommentsFeed {
+        static var title: String {
+            return get(key: "IMAGE_COMMENTS_VIEW_TITLE")
+        }
+        
+        private static func get(key: String) -> String {
+            NSLocalizedString(key,
+                        tableName: table,
+                        bundle: Bundle(for: R.self),
+                        comment: "")
+        }
+    }
+}
--- EssentialCommentsFeed/EssentialCommentsFeedTests/Feed Presentation/ImageCommentsFeedViewModelTests.swift
+++ EssentialCommentsFeed/EssentialCommentsFeedTests/Feed Presentation/ImageCommentsFeedViewModelTests.swift
@@ -5,3 +5,30 @@
 //  Created by Wilmer Barrios on 03/04/21.
 //
 
+import Foundation
+import XCTest
+@testable import EssentialCommentsFeed
+
+public final class ImageCommentsViewModel {
+    public static var title: String {
+        return R.ImmageCommentsFeed.title
+    }
+}
+
+class ImageCommentsFeedViewModelTests: XCTestCase {
+    func test_title_isLocalized() {
+        XCTAssertEqual(ImageCommentsViewModel.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
+    }
+    
+    // MARK: Helpers
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "ImageComments"
+//        let bundle = Bundle(for: ImageCommentsViewModel.self)
+        let bundle = Bundle(for: R.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}
