2dc4f14db7a0e21da16264e211ceaa003e8e92b5
Wilmer Barrios <wilmerbt@yahoo.com>
2021-04-04 19:35:43 -0500

    add `ImageCommentCell` with storyboard connections

---
+++ EssentialCommentsFeedPrototype/EssentialCommentsFeedPrototype/ImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentCell.swift
+//  EssentialCommentsFeedPrototype
+//
+//  Created by Wilmer Barrios on 04/04/21.
+//
+
+import Foundation
+import UIKit
+
+final class ImageCommentCell: UITableViewCell {
+    @IBOutlet weak var authorNameLabel: UILabel!
+    @IBOutlet weak var createdAtLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+}
--- EssentialCommentsFeedPrototype/EssentialCommentsFeedPrototype/ImageCommentsFeedController.swift
+++ EssentialCommentsFeedPrototype/EssentialCommentsFeedPrototype/ImageCommentsFeedController.swift
@@ -16,7 +16,7 @@ struct ImageCommentsFeedViewModel {
 
 class ImageCommentsFeedController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return ImageCommentsFeedViewModel.prototypedFeed.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
