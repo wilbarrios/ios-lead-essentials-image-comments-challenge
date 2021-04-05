ddc2e2f423434ed5f03b1c390fa55e213dc6a978
Wilmer Barrios <wilmerbt@yahoo.com>
2021-04-05 18:59:34 -0500

    `ImageCommentsFeedController` init does not load comments

--- EssentialCommentsFeed/EssentialCommentsFeediOSTests/ImageCommentsFeedControllerTests.swift
+++ EssentialCommentsFeed/EssentialCommentsFeediOSTests/ImageCommentsFeedControllerTests.swift
@@ -5,3 +5,27 @@
 //  Created by Wilmer Barrios on 05/04/21.
 //
 
+import Foundation
+import XCTest
+
+final class ImageCommentsFeedController {
+    init(loader: ImageCommentsFeedControllerTests.CommentsLoaderMock) {
+        
+    }
+}
+
+class ImageCommentsFeedControllerTests: XCTestCase {
+    
+    func test_init_doesNotLoadComments() {
+        let loader = CommentsLoaderMock()
+        let _ = ImageCommentsFeedController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCounts, 0)
+    }
+    
+    // MARK: Testing entities
+    class CommentsLoaderMock {
+        private(set) var loadCallCounts: Int = 0
+    }
+    
+}
